ac45ff7c8885507f0c2bdcd70090d85d9b441222
mathiasbredholt <mathiasbredholt@gmail.com>
2020-09-18 22:15:37 +0200

    Updated example

--- main/main.cpp
+++ main/main.cpp
@@ -14,19 +14,24 @@ main
 
 */
 
+#include <iostream>
+
 #include "MapLooper/MapLooper.hpp"
 #include "esp_event.h"
-#include "esp_ota_ops.h"
+#include "esp_log.h"
+#include "esp_wifi.h"
+#include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "nvs_flash.h"
 #include "protocol_examples_common.h"
 
 static const char* TAG = "main";
 
-extern "C" void app_main() {
-  ESP_LOGI(TAG, "MapLooper v%s", esp_ota_get_app_description()->version);
-  ESP_LOGI(TAG, "(c) Mathias Bredholt 2020");
+void updateParam(int id, const std::string& path, float value) {
+  std::cout << path << " : " << value << '\n';
+}
 
+extern "C" void app_main() {
   // Connect to WiFi
   ESP_ERROR_CHECK(nvs_flash_init());
   ESP_ERROR_CHECK(esp_netif_init());
@@ -34,7 +39,13 @@ extern "C" void app_main() {
   ESP_ERROR_CHECK(example_connect());
 
   MapLooper::MapLooper* mapLooper = new MapLooper::MapLooper();
-  mapLooper->addSignal(
-      "gate", 0, 1, [](int trackId, const std::string& path, float value) {});
-  vTaskDelete(NULL);
+
+  mapLooper->addSignal("testSignal1", 0.0f, 1.0f, updateParam);
+  mapLooper->addSignal("testSignal2", 0.0f, 1.0f, updateParam);
+
+  mapLooper->autoMap();
+
+  for (;;) {
+    vTaskDelay(portMAX_DELAY);
+  }
 }
